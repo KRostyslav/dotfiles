2aa30c76fe88b32c5ca565fe74c269af818b434e
krostyslav <rostyslav.kryvyy@gmail.com>
2021-06-19 16:32:17 +0300

    add install dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -3,11 +3,16 @@
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # vim
-ln -s ${BASEDIR}/vimrc ~/.vimrc
-ln -s ${BASEDIR}/vim/ ~/.vim
+ln -s ${BASEDIR}/.vimrc ~/.vimrc
+echo "Added vimrc"
+
+ln -s ${BASEDIR}/.vim/ ~/.vim
+echo "Added vim"
 
 # zsh
-ln -s ${BASEDIR}/zshrc ~/.zshrc
+ln -s ${BASEDIR}/.zshrc ~/.zshrc
+echo "Added zshrc"
 
 # git
-ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
+echo "Added gitconfig"
